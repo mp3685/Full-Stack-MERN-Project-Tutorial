9c779dcc9d7ad6ec3e8e3c3795ab3f69aa3d7092
treesus-christ <mjpersad@gmail.com>
2023-06-24 14:04:59 -0400

    Fixed Client Side For Google API Token
    Fixed the client side code to properly populate the Google API Token variable

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { React, useState, useEffect } from 'react';
+import { React } from 'react';
 import { Container } from '@material-ui/core';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { GoogleOAuthProvider } from '@react-oauth/google';
@@ -7,15 +7,12 @@ import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
 import Auth from './components/Auth/Auth';
 
+import { fetchGoogleAPI } from './api/index.js';
+
+const data = await fetchGoogleAPI();
+const googleAPIKey = data.data.data;
+
 const App = () => {
-    const [googleAPIKey, setGoogleAPIKey] = useState(null);
-
-    useEffect(() => {
-        fetch('/api').then((res) => res.json()).then((data) => {
-            setGoogleAPIKey(data.data);
-        }).catch(console.error);
-    }, []);
-
     return (
         <GoogleOAuthProvider clientId={`${googleAPIKey}`}>
             <BrowserRouter>
--- client/src/api/index.js
+++ client/src/api/index.js
@@ -23,3 +23,5 @@ export const likePost = (id) => API.patch(`/posts/${id}/likePost`);
 
 export const signIn = (formData) => API.post('/user/signin', formData);
 export const signUp = (formData) => API.post('/user/signup', formData);
+
+export const fetchGoogleAPI = () => API.get('/api');
